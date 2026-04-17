77d0c779aff43d22dab657039278f1ee67314ba1
Nazhif Alhuwaidie <nazhifalhuwaidie12@gmail.com>
2026-04-17 14:47:42 +0700

    feat: next.config.js

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        // 🔥 Panggil dari brankas .env kamu!
+        
         destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`, 
       },
     ]
