dc27b1e4a468624f441f135d3abce02482b50a4f
Nazhif Alhuwaidie <nazhifalhuwaidie12@gmail.com>
2026-05-19 16:01:49 +0700

    feat: new branch

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   // Langsung arahkan user ke halaman login saat buka route '/'
-  redirect('/Login');
+  redirect('/login');
 }
